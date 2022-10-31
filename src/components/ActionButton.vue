2f36e63d19e2dad29afa366b00ff869e3ff457f7
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-10-31 09:35:06 +0100

    first commit

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button :class="buttonClass">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: ["text"],
+  data() {
+    return {
+      primary: true,
+    };
+  },
+  computed: {
+    buttonClass() {
+      return {
+        primary: this.primary,
+      };
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 rounded font-medium;
+}
+
+.primary {
+  @apply text-white border-0 bg-brand-blue-1 hover:bg-blue-800 hover:shadow-blue;
+}
+
+.secondary {
+}
+</style>
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="imageLink"
+    alt="Profile Image"
+    class="w-10 h-10 object-cover rounded-3xl"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://images.pexels.com/photos/6282045/pexels-photo-6282045.jpeg",
+    };
+  },
+};
+</script>
