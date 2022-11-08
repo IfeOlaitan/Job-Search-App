50ebbef41fa1974803262b26812115175984093b
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-11-08 20:17:01 +0100

    hero section

---
+++ src/components/Headline.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <h1
+      class="font-bold text-8xl tracking-tighter mb-14"
+      data-test="action-phrase"
+    >
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
+    ,
+    <h2 class="text-3xl font-light">Find your next jov at tech careers</h2>
+  </section>
+</template>
+
+<script>
+import nextElementInList from "@/utils/nextElementInList";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Headline",
+  data() {
+    return {
+      action: "Build",
+      interval: null,
+    };
+  },
+  computed: {
+    actionClasses() {
+      return {
+        [this.action.toLowerCase()]: true,
+      };
+    },
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        this.action = nextElementInList(actions, this.action);
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
