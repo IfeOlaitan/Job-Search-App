2f52c07ba3ff3b5b11523bab508d325f39e83787
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-11-21 20:39:23 +0100

    job filters sidebar and teams page

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,37 @@
+<template>
+  <accordian header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-wrap">
+          <li class="w-1/2 h-8">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </accordian>
+</template>
+
+<script>
+import Accordian from "@/components/Shared/Accordian.vue";
+
+export default {
+  name: "JobFiltersSidebarOrganizations",
+  components: {
+    Accordian,
+  },
+};
+</script>
