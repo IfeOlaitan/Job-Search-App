c41ad18d187c28ad5b570dcc1f216c886ec656f9
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-11-22 18:34:51 +0100

    filtering jobs

--- src/components/Navigation/Subnav.vue
+++ src/components/Navigation/Subnav.vue
@@ -3,17 +3,27 @@
     <div class="flex items-center h-full px-8">
       <div v-if="onJobsResultsPage" data-test="job-count">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-        <span><span class="text-brand-green-1">1203</span> jobs matched</span>
+        <span
+          ><span class="text-brand-green-1">{{ numberOfJobs }}</span> jobs
+          matched</span
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { FILTERED_JOBS_BY_ORGANIZATIONS } from "@/store";
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Subnav",
   computed: {
+    ...mapGetters([FILTERED_JOBS_BY_ORGANIZATIONS]),
+    numberOfJobs() {
+      return this.FILTERED_JOBS_BY_ORGANIZATIONS.length;
+    },
     onJobsResultsPage() {
       return this.$route.name === "JobResults";
     },
